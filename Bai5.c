4dc0662c4dd2f4b29bc8be657ffdc78fc520b0d1
phuong <phuongtk0407>
2025-07-14 16:20:40 +0700

    Bai 5

---
+++ Bai5.c
@@ -0,0 +1,21 @@
+//Bai 5: Dem so luong chu so cua so nguyen duong n
+#include <stdio.h>
+int main() {
+    int n, count=0;
+
+    printf("Nhap so n: ");
+    scanf("%d", &n);
+
+   if (n == 0) {
+        count = 1; // Số 0 có 1 chữ số
+    } else {
+        int temp = n; 
+        while (temp > 0) {
+            temp = temp / 10;
+            count++;
+        }
+    }
+
+    printf("Số lượng chữ số của %d là: %d\n", n, count);
+    return 0;
+}
